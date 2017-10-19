e39a130a83a795d4aa0c200c9ac3a1666dd651bd
Fernando <fernandoza@gmail.com>
2017-10-19 13:19:21 -0500

    Adds hello to the url.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "hello", urlPatterns = "/")
+@WebServlet(name = "hello", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(
         HttpServletRequest request, //input
